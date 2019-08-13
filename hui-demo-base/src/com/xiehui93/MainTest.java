4fa856d06175c984dc8f0dc9abde307a8857c20a
xiehui <xiehui93@qq.com>
2019-08-13 15:45:32 +0800

    基础知识项目提交

---
+++ hui-demo-base/src/com/xiehui93/MainTest.java
@@ -0,0 +1,16 @@
+package com.xiehui93;
+/**
+ * 
+ * @ClassName: MainTest 
+ * @Description: TODO(这里用一句话描述这个类的作用) 
+ * @author xiehui 
+ * @date 2019年8月13日 下午3:36:49
+ */
+public class MainTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
